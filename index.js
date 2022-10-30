a6e41b585e32b51d1015903b40a042d5045ea33e
etoyadadaya <vanyasoup2014@cyber.sibsutis.ru>
2022-10-30 18:18:23 +0700

    intermediate handlers

--- index.js
+++ index.js
@@ -4,11 +4,23 @@ import { userRouter } from "./users/users.js";
 const port = 8000;
 const app = express();
 
+app.use((req, res, next) => {
+    console.log(`time `, Date.now());
+    next();
+});
+
 app.get('/', (req, res) => {
-    res.sendStatus(404).end();
+    // res.sendStatus(404).end();
+    throw new Error("ERRORROROORR");
+
 });
 
 app.use('/users', userRouter);
+
+app.use((err, req, res, next) => {
+    console.log(err.message);
+    res.status(500).send(err.message);
+});
 
 app.listen(port, () => {
     console.log(`server started on http://localhost:${port}`);
