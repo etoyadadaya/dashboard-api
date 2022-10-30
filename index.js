9053ee7e39ed418b819f10501540eebe0d94abb6
etoyadadaya <vanyasoup2014@cyber.sibsutis.ru>
2022-10-30 18:07:22 +0700

    Router

--- index.js
+++ index.js
@@ -1,33 +1,14 @@
 import express from 'express';
+import { userRouter } from "./users/users.js";
 
 const port = 8000;
 const app = express();
 
 app.get('/', (req, res) => {
-    // res.status(201).json({ success: true });
-    // res.download('/test.pdf', 'TEST.pdf');
-    // res.redirect(301, 'https://google.com');
-    // res.set('Content-Type', 'text/plain');
-    // res.append('Warning', 'code');
-    // res.type('application/json');
-    // res.location('https://aboba.com');
-
-    // res.links({
-    //     next: "https://"
-    // });
-
-    // res.cookie('token', 'aboba', {
-    //     domain: '',
-    //     path: '/',
-    //     secure: true,
-    //     expires: 600000
-    // });
-    // res.clearCookie('token');
-
-    // res.send('Hello');
-
-    // res.sendStatus(404).end();
+    res.sendStatus(404).end();
 });
+
+app.use('/users', userRouter);
 
 app.listen(port, () => {
     console.log(`server started on http://localhost:${port}`);
