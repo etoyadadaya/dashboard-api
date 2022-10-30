1fdf96d89625c09e1f8a794af2299f7dbb192e11
etoyadadaya <vanyasoup2014@cyber.sibsutis.ru>
2022-10-30 17:47:12 +0700

    Simple routing

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ import express from 'express';
 const port = 8000;
 const app = express();
 
-app.get('/', (req, res) => {
+app.all('/', (req, res, next) => {
+    console.log('All');
+    next();
+});
+
+const cb = (req, res, next) => {
+    console.log('cb');
+    next();
+};
+
+app.get('/', cb, (req, res) => {
     res.send('Hello');
 });
 
