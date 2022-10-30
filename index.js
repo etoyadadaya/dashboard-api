ac80aae856e303956420348bfeb0c5d2478c6e6d
etoyadadaya <vanyasoup2014@cyber.sibsutis.ru>
2022-10-30 17:59:11 +0700

    responses to the user

--- index.js
+++ index.js
@@ -3,18 +3,30 @@ import express from 'express';
 const port = 8000;
 const app = express();
 
-app.all('/', (req, res, next) => {
-    console.log('All');
-    next();
-});
+app.get('/', (req, res) => {
+    // res.status(201).json({ success: true });
+    // res.download('/test.pdf', 'TEST.pdf');
+    // res.redirect(301, 'https://google.com');
+    // res.set('Content-Type', 'text/plain');
+    // res.append('Warning', 'code');
+    // res.type('application/json');
+    // res.location('https://aboba.com');
 
-const cb = (req, res, next) => {
-    console.log('cb');
-    next();
-};
+    // res.links({
+    //     next: "https://"
+    // });
 
-app.get('/', cb, (req, res) => {
-    res.send('Hello');
+    // res.cookie('token', 'aboba', {
+    //     domain: '',
+    //     path: '/',
+    //     secure: true,
+    //     expires: 600000
+    // });
+    // res.clearCookie('token');
+
+    // res.send('Hello');
+
+    // res.sendStatus(404).end();
 });
 
 app.listen(port, () => {
