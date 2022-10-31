0b44cf9df39acfc719bba3916e863850db1ae2be
etoyadadaya <vanyasoup2014@cyber.sibsutis.ru>
2022-10-31 20:00:15 +0700

    Error exception filter implementation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
 import { BaseController } from "../common/base.controller";
 import { LoggerService } from "../logger/logger.service";
 import { NextFunction, Request, Response } from "express";
+import {HTTPError} from "../errors/http-error.class";
 
 export class UserController extends BaseController {
     constructor(
@@ -14,11 +15,10 @@ export class UserController extends BaseController {
     }
 
     login(req: Request, res: Response, next: NextFunction) {
-        this.ok(res, 'login');
+        next(new HTTPError(401, 'auth error', 'login'));
     }
 
     register(req: Request, res: Response, next: NextFunction) {
         this.ok(res, 'register');
     }
 }
-
