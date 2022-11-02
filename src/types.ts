57c6c350d12d5922aa3b78becaf9e3df5f03bf03
etoyadadaya <vanyasoup2014@cyber.sibsutis.ru>
2022-11-02 15:05:09 +0700

    Added sqlite and prisma

--- src/types.ts
+++ src/types.ts
@@ -5,4 +5,5 @@ export const TYPES = {
 	UserService: Symbol.for('UserService'),
 	ConfigService: Symbol.for('ConfigService'),
 	ExceptionFilter: Symbol.for('ExceptionFilter'),
+	PrismaService: Symbol.for('PrismaService'),
 };
