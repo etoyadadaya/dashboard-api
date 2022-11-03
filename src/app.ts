943777c02eacc4a32e6d72e9acfb6d5fe7e20bc5
etoyadadaya <vanyasoup2014@cyber.sibsutis.ru>
2022-11-03 17:33:02 +0700

    Jest, Register e2e test

--- src/app.ts
+++ src/app.ts
@@ -50,4 +50,8 @@ export class App {
 		this.server = this.app.listen(this.port);
 		this.logger.log(`server started on http://localhost:${this.port}`);
 	}
+
+	public close(): void {
+		this.server.close();
+	}
 }
