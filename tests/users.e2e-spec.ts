f0a81c5bfa0076841a3c30a4dad68f40ed1ee208
etoyadadaya <vanyasoup2014@cyber.sibsutis.ru>
2022-11-03 18:09:53 +0700

    Jest, e2e tests, all covered

--- tests/users.e2e-spec.ts
+++ tests/users.e2e-spec.ts
@@ -10,12 +10,56 @@ beforeAll(async () => {
 });
 
 describe('Users e2e', () => {
+	it('Register - success', async () => {
+		const res = await request(application.app).post('/users/register').send({
+			email: 'a@a.ru',
+			password: '1',
+		});
+		expect(res.body.jwt).not.toBeUndefined();
+	});
+
 	it('Register - error', async () => {
 		const res = await request(application.app).post('/users/register').send({
 			email: 'a@a.ru',
 			password: '1',
 		});
 		expect(res.statusCode).toBe(422);
+	});
+
+	it('Login - success', async () => {
+		const res = await request(application.app).post('/users/login').send({
+			email: 'aboba@gmail.com',
+			password: 'aboba',
+		});
+		expect(res.body.jwt).not.toBeUndefined();
+	});
+
+	it('Login - error', async () => {
+		const res = await request(application.app).post('/users/login').send({
+			email: 'aboba@gmail.com',
+			password: '1',
+		});
+		expect(res.statusCode).toBe(401);
+	});
+
+	it('Info - success', async () => {
+		const login = await request(application.app).post('/users/login').send({
+			email: 'aboba@gmail.com',
+			password: 'aboba',
+		});
+		const res = await request(application.app)
+			.get('/users/info')
+			.set('Authorization', `Bearer ${login.body.jwt}`);
+		expect(res.body.email).toBe('aboba@gmail.com');
+	});
+
+	it('Info - error', async () => {
+		const login = await request(application.app).post('/users/login').send({
+			email: 'aboba@gmail.com',
+			password: 'aboba',
+		});
+		const res = await request(application.app).get('/users/info').set('Authorization', `Bearer 1`);
+		expect(res.statusCode).toBe(401);
 	});
 });
 
